d8fdc94c7117a5baf9b28db3922b5e495cece5c7
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2021-04-27 09:43:42 +0100

    listen on 0.0.0.0 instead of localhost

--- bin/paas-wrapper.sh
+++ bin/paas-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export DOWNSTREAM__IP=localhost
+export DOWNSTREAM__IP=0.0.0.0
 export DOWNSTREAM__PORT=5432
 
 export UPSTREAM__HOST=$(echo $VCAP_SERVICES | jq -r '.["postgres"][0].credentials.host')
